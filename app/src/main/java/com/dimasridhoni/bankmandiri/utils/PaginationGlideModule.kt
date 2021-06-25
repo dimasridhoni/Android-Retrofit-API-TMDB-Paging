d3b843ac307d17e3077c8c9fb92603922d50bb88
DimasRidho <dimasridhoni@gmail.com>
2021-06-25 22:08:46 +0800

    Autoplay Youtube

--- app/src/main/java/com/dimasridhoni/bankmandiri/utils/PaginationGlideModule.kt
+++ app/src/main/java/com/dimasridhoni/bankmandiri/utils/PaginationGlideModule.kt
@@ -3,27 +3,22 @@
  * Copyright (c) 2021 . All rights reserved.
  * Last modified 6/25/21 11:11 PM
  */
+package com.dimasridhoni.bankmandiri.utils
 
-package com.dimasridhoni.bankmandiri.utils;
-
-import android.content.Context;
-import androidx.annotation.NonNull;
-import com.bumptech.glide.GlideBuilder;
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.module.AppGlideModule;
-import com.bumptech.glide.request.RequestOptions;
+import android.content.Context
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
 
 @GlideModule
-public class PaginationGlideModule extends AppGlideModule {
-
-    @Override
-    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
-        super.applyOptions(context, builder);
+class PaginationGlideModule : AppGlideModule() {
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
         builder.setDefaultRequestOptions(
-                new RequestOptions()
-                        .diskCacheStrategy(DiskCacheStrategy.ALL) // cache both original & resized image
-        );
+            RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.ALL) // cache both original & resized image
+        )
     }
-
 }
